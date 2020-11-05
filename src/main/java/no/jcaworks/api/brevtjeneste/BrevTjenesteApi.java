76b911ab222ce62b1906846649f55963ccfd6e48
Jan Christian Alvestad <jan.christian.alvestad@tribia.com>
2020-11-05 01:38:27 +0100

    Ability to configure host + port for external systems

--- src/main/java/no/jcaworks/api/brevtjeneste/BrevTjenesteApi.java
+++ src/main/java/no/jcaworks/api/brevtjeneste/BrevTjenesteApi.java
@@ -1,23 +1,36 @@
 package no.jcaworks.api.brevtjeneste;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Component
-public class BrevTjenesteApi {
+public class BrevTjenesteApi implements InitializingBean {
 
-    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://127.0.0.1:1088")
-            .addConverterFactory(JacksonConverterFactory.create())
-            .client(httpClient.build())
-            .build();
+    @Value("${brevtjeneste.host}")
+    private String host;
+    @Value("${brevtjeneste.port}")
+    private String port;
+
+    OkHttpClient.Builder httpClient;
+    Retrofit retrofit;
 
     @Bean
     public BrevTjenesteResource createBrevTjenesteResource() {
         return retrofit.create(BrevTjenesteResource.class);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        httpClient = new OkHttpClient.Builder();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + host + ":" + port)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
     }
 }
--- src/main/java/no/jcaworks/api/fagsystem/FagSystemApi.java
+++ src/main/java/no/jcaworks/api/fagsystem/FagSystemApi.java
@@ -1,23 +1,36 @@
 package no.jcaworks.api.fagsystem;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Component
-public class FagSystemApi {
+public class FagSystemApi implements InitializingBean {
 
-    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://127.0.0.1:1087")
-            .addConverterFactory(JacksonConverterFactory.create())
-            .client(httpClient.build())
-            .build();
+    @Value("${fagsystem.host}")
+    private String host;
+    @Value("${fagsystem.port}")
+    private String port;
+
+    OkHttpClient.Builder httpClient;
+    Retrofit retrofit;
 
     @Bean
     public FagSystemResource createFagsystemResource() {
         return retrofit.create(FagSystemResource.class);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        httpClient = new OkHttpClient.Builder();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + host + ":" + port)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
     }
 }
