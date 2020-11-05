0b789dd97ba7a9e474e0a0d96f67448d86420b00
Jan Christian Alvestad <jan.christian.alvestad@tribia.com>
2020-11-05 03:14:27 +0100

    Add validation for client facing dto's

--- src/main/java/no/jcaworks/controllers/agreement/dto/CreateAgreementRequest.java
+++ src/main/java/no/jcaworks/controllers/agreement/dto/CreateAgreementRequest.java
@@ -5,6 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 /**
  * Request object for create agreement
  */
@@ -15,11 +20,24 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CreateAgreementRequest {
 
+    @NotEmpty(message = "Vehicle registration number is required")
     private String vehicleRegistrationNumber;
+
+    @Min(value = 0, message = "Bonus cannot be lower than 0")
+    @Max(value = 100, message = "Bonus cannot be higher than 100")
     private Integer bonus;
+
+    @NotEmpty(message = "Social security number must be provided")
     private String socialSecurityNumber;
+
+    @NotEmpty(message = "First name is required")
     private String firstName;
+
+    @NotEmpty(message = "Last name is required")
     private String lastName;
+
+    @NotEmpty(message = "Email is required")
+    @Pattern(regexp = "^(.+)@(.+)$\"", message = "Email is invalid")
     private String email;
 
 
